bbe5f3e4aa75cbd12df07335bb572ae438aa87df
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-10-29 01:08:45 +0530

    DB update:

--- web/connection.php
+++ web/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-define('servername', 'ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('user', 's1fik9aru9o2b7an');
-define('password', 'ldtbatqicw6ex2y9');
-define('database', 'n7zptp6z8wn8v3qs');
+define('servername', 'sql6.freemysqlhosting.net');
+define('user', 'sql6447503');
+define('password', 'Z8mfBdVaII');
+define('database', 'sql6447503');
 
 $conn = mysqli_connect(servername, user, password, database) or die("Connection failed: " . $conn->connect_error);
 
