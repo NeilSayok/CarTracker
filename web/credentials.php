908d3780ee2e367ba8ecd1d52a7965578a7a7bc1
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-10-31 12:03:48 +0530

    Updated Credentials

--- web/credentials.php
+++ web/credentials.php
@@ -5,8 +5,8 @@ define("password", "test@1234");
 define('smtp', 'smtp://c34a731b2f948c50:vWPgydr2raifrhY8APZJHs5C@smtp.cloudmta.net:587?starttls=true');
 
 
-$hostname = 'hostname from account';
-$username = 'username from account';
-$password = 'password from account';
+$hostname = 'smtp.cloudmta.net';
+$username = 'c34a731b2f948c50';
+$password = 'vWPgydr2raifrhY8APZJHs5C';
 
 
