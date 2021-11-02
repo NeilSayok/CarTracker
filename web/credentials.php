ff6a8af12d01512ac2ad2e7630a4135bf3ee794b
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-11-02 07:16:00 +0530

    Revert back to working mail code

--- web/credentials.php
+++ web/credentials.php
@@ -5,16 +5,17 @@
 // define('smtp', 'smtp://c34a731b2f948c50:vWPgydr2raifrhY8APZJHs5C@smtp.cloudmta.net:587?starttls=true');
 
 
-// $hostname = 'smtp.cloudmta.net';
-// $username = 'c34a731b2f948c50';
-// $password = 'vWPgydr2raifrhY8APZJHs5C';
-
-// define("smtp_server", "smtp.mailgun.org");
-// define("login", "postmaster@sandboxad7c11c744684607a80388818e661a48.mailgun.org");
-// define("password", "958a3bc12476c88a01bc2423267ed304-10eedde5-fd924c62");
+$hostname = 'smtp.cloudmta.net';
+$username = 'c34a731b2f948c50';
+$password = 'vWPgydr2raifrhY8APZJHs5C';
 
 
 
-define("smtp_server", "smtp-relay.sendinblue.com");
-define("userid", "blue.labs.dev@gmail.com");
-define("password", "ULHYfM0nOvVm5EDF");
+// define("smtp_server", "smtp-relay.sendinblue.com");
+// define("login", "sdmsdm1998@gmail.com");
+// define("password", "B57NQpsHZmKUfLA8");
+
+
+define("smtp_server", "smtp.mailgun.org");
+define("login", "postmaster@sandboxad7c11c744684607a80388818e661a48.mailgun.org");
+define("password", "958a3bc12476c88a01bc2423267ed304-10eedde5-fd924c62");
