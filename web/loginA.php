1e82d18df6ac543365746c8f48e7ed77dda21ce6
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-11-02 09:11:43 +0530

    update 3

--- web/loginA.php
+++ web/loginA.php
@@ -8,31 +8,27 @@
 require_once 'connection.php';
 
 
-if ($conn->connect_error)
-{
-    die("Connection failed: " . $conn->connect_error);
-}else{
-    $inpemail = $_POST['email'];
-    $inppsw = $_POST['psw'];
 
-    $query = "SELECT * FROM `car_location` WHERE `email` = '".$inpemail."' OR `reg_id` = '".$inpemail."'";
+$inpemail = $_POST['email'];
+$inppsw = $_POST['psw'];
 
-    if($result = mysqli_query($conn,$query)){
-        $row = mysqli_fetch_array($result);
+$query = "SELECT * FROM `car_location` WHERE `email` = '".$inpemail."' OR `reg_id` = '".$inpemail."'";
+
+if($result = mysqli_query($conn,$query)){
+    $row = mysqli_fetch_array($result);
+    
+    if(count($row) > 0)
+        if($row['password'] == $inppsw){
+            $array = array('name' => $row['name'],'reg_id' => $row['reg_id'],'email' => $row['email'], 'verified' => $row['verified'],'status' => 'OK');
+        }else{
+            $array = array('status' => 'passMiss','email'=> $inpemail);
+        }
+    else{
+            $array = array('status' => 'credMiss','email'=> $inpemail);
+        } 
         
-        if(count($row) > 0)
-           if($row['password'] == $inppsw){
-                $array = array('name' => $row['name'],'reg_id' => $row['reg_id'],'email' => $row['email'], 'verified' => $row['verified'],'status' => 'OK');
-           }else{
-             $array = array('status' => 'passMiss','email'=> $inpemail);
-           }
-        else{
-                $array = array('status' => 'credMiss','email'=> $inpemail);
-            } 
-            
-            echo json_encode($array);
-
-    }
+        echo json_encode($array);
 
 }
+
 ?>
