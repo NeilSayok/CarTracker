a7d3366b61685ee44dad200fe9a08904ac48f67a
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-11-02 07:35:24 +0530

    update 3

--- web/credentials.php
+++ web/credentials.php
@@ -5,9 +5,9 @@
 // define('smtp', 'smtp://c34a731b2f948c50:vWPgydr2raifrhY8APZJHs5C@smtp.cloudmta.net:587?starttls=true');
 
 
-$hostname = 'smtp.cloudmta.net';
-$username = 'c34a731b2f948c50';
-$password = 'vWPgydr2raifrhY8APZJHs5C';
+// $hostname = 'smtp.cloudmta.net';
+// $username = 'c34a731b2f948c50';
+// $password = 'vWPgydr2raifrhY8APZJHs5C';
 
 
 
@@ -16,6 +16,6 @@ $password = 'vWPgydr2raifrhY8APZJHs5C';
 // define("password", "B57NQpsHZmKUfLA8");
 
 
-define("smtp_server", "smtp.mailgun.org");
-define("login", "postmaster@sandboxad7c11c744684607a80388818e661a48.mailgun.org");
-define("password", "958a3bc12476c88a01bc2423267ed304-10eedde5-fd924c62");
+$smtp_server= "smtp.mailgun.org";
+$login= "postmaster@sandboxad7c11c744684607a80388818e661a48.mailgun.org";
+$password= "958a3bc12476c88a01bc2423267ed304-10eedde5-fd924c62";
