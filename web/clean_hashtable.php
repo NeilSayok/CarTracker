458422afe5a11c8bd3b930c044b83d29d933329e
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-11-04 03:53:12 +0530

    1.1.15

--- web/clean_hashtable.php
+++ web/clean_hashtable.php
@@ -4,5 +4,5 @@ require_once 'connection.php';
 require_once 'credentials.php';
 require_once 'response.php';
 
-$query = "DELETE FROM temp_hash WHERE timestamp <= UNIX_TIMESTAMP() - 86400";
+$query = "DELETE FROM temp_hash WHERE timestamp <= UNIX_TIMESTAMP() - 3600";
 mysqli_query($conn,$query);
