b4d7b3b5572e7e18bb5d864805b3608f037a695a
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-11-06 10:14:07 +0530

    1.3.1

--- web/resend_otp.php
+++ web/resend_otp.php
@@ -5,65 +5,71 @@ require_once 'credentials.php';
 
 require '../vendor/autoload.php';
 
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
-    $variable = array();
-    $variable['email'] = $_POST['email'];
-    $variable['name'] = "";
-    $variable['otp'] = "";
-    $variable['reg_id'] = "";
+$out_arr = array("response"=>"",
+"code" => "",
+);
 
-    $variable['name'] = getName($variable['email']);
-    $variable['reg_id'] = getVehId($variable['email']);
+$variable = array();
+$variable['email'] = $_POST['email'];
+$variable['name'] = "";
+$variable['otp'] = "";
+$variable['reg_id'] = "";
+
+$variable['name'] = getName($variable['email']);
+$variable['reg_id'] = getVehId($variable['email']);
+$variable['otp'] = mt_rand(100000, 999999);
+while (isOTPPresent($variable['otp'])) {
     $variable['otp'] = mt_rand(100000, 999999);
-    while (isOTPPresent($variable['otp'])) {
-        $variable['otp'] = mt_rand(100000, 999999);
-    }
+}
 
-    $email = $variable['email'] ;
-    $name =  $variable['name'];
-    $otp = $variable['otp'];
-    $vehid = $variable['reg_id'];
+$email = $variable['email'] ;
+$name =  $variable['name'];
+$otp = $variable['otp'];
+$vehid = $variable['reg_id'];
 
-    $query = "UPDATE `OTP` SET `otp` = '".$otp."' WHERE `OTP`.`email` = '".$email."'";
+$query = "UPDATE `OTP` SET `otp` = '".$otp."' WHERE `OTP`.`email` = '".$email."'";
 
-    mysqli_query($conn, $query);
-    //echo "Success<br>";
+mysqli_query($conn, $query);
+//echo "Success<br>";
 
-    //echo $email."<br>".$name."<br>".$otp;
+//echo $email."<br>".$name."<br>".$otp;
 
 
-    $template = file_get_contents("email.html");
+$template = file_get_contents("email.html");
 
-    $template = str_replace('##% name %##', $variable['name'], $template);
-    $template = str_replace('##% otp %##', $variable['otp'], $template);
-    $template = str_replace('##% email %##', $variable['email'], $template);
-    $template = str_replace('##% regid %##', $variable['reg_id'], $template);
+$template = str_replace('##% name %##', $variable['name'], $template);
+$template = str_replace('##% otp %##', $variable['otp'], $template);
+$template = str_replace('##% email %##', $variable['email'], $template);
+$template = str_replace('##% regid %##', $variable['reg_id'], $template);
 
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
-    $mail->Host = $smtp_server;// use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
-    $mail->Username = $login;
-    $mail->Password = $password;
-    $mail->Port = 587; // TLS only
-    $mail->SMTPSecure = 'tls'; // ssl is deprecated
-    $mail->SMTPAuth = true;
-    $mail->setFrom($sent_from_emial, $sent_from_name); // From email and name
-    $mail->addAddress($variable['email'], $variable['name']); // to email and name
-    $mail->Subject = 'Registration OTP (Do not share!)';
-    $mail->msgHTML($template); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
-    $mail->AltBody = 'Your OTP is '.$variable['otp']; // If html emails is not supported by the receiver, show this body
-    // $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+$mail->Host = $smtp_server;// use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+$mail->Username = $login;
+$mail->Password = $password;
+$mail->Port = 587; // TLS only
+$mail->SMTPSecure = 'tls'; // ssl is deprecated
+$mail->SMTPAuth = true;
+$mail->setFrom($sent_from_emial, $sent_from_name); // From email and name
+$mail->addAddress($variable['email'], $variable['name']); // to email and name
+$mail->Subject = 'Registration OTP (Do not share!)';
+$mail->msgHTML($template); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+$mail->AltBody = 'Your OTP is '.$variable['otp']; // If html emails is not supported by the receiver, show this body
+// $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
 
-    if (!$mail->send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-        echo "Mail_not_sent";
-    } else {
-        echo "Mail_Sent";
-    }
+if (!$mail->send()) {
+    $out_arr["response"] = "Mail_not_sent";
+    $out_arr["code"] = 301;
+} else {
+    $out_arr["response"] = "Mail_sent";
+    $out_arr["code"] = 300;
+}
 
 
 
