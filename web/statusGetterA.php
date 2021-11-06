2cce17d3ef2c89f3f345c0b9f97cfb70e77a50d7
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-11-06 17:47:41 +0530

    2.0.4

--- web/statusGetterA.php
+++ web/statusGetterA.php
@@ -14,9 +14,6 @@ require_once 'response.php';
     
 $inpEmailList = $_POST['emails'];
 
-echo $inpEmailList;
-
-
 // $inp_arr = json_decode($inpEmailList,true);
 
 $query = "SELECT * FROM car_location WHERE `email` in (";
@@ -26,37 +23,38 @@ foreach($inpEmailList as $email){
 }
 $query = rtrim($query,',');
 $query .= ")";
-echo $query;
+
 
 // $query = "SELECT `email`,`log_stat` FROM car_location WHERE `email` = '".$inpEmailList."'";
 
-/*$sql =  mysqli_query($conn, $query);
+
+$sql =  mysqli_query($conn, $query);
 $row = mysqli_fetch_array($sql);
-echo $row['log_stat'];*/
-/*
-$emailarr = explode('#!!#',$inpEmailList);
+// echo $row['log_stat'];
+
+// $emailarr = explode('#!!#',$inpEmailList);
 
 
-$arr = array();
+// $arr = array();
 
-$out_arr = array("resposnse"=>null,
-"code" => null,
-"data"=>null,
+$out_arr = array("resposnse"=>"stat_online",
+"code" => 500,
+"data"=>json_encode($row),
 );
 
 
 
-foreach($emailarr as $email){
-    $query = "SELECT `log_stat` FROM car_location WHERE `email` = '".$email."'";
-    $sql =  mysqli_query($conn, $query);
-    $row = mysqli_fetch_array($sql);
+// foreach($emailarr as $email){
+//     $query = "SELECT `log_stat` FROM car_location WHERE `email` = '".$email."'";
+//     $sql =  mysqli_query($conn, $query);
+//     $row = mysqli_fetch_array($sql);
     
-    array_push($arr,$row['log_stat']);
+//     array_push($arr,$row['log_stat']);
     
-}
+// }
 
 echo json_encode($arr);
-    */
+    
     
     
     
