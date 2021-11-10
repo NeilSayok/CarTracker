b41a18b610af2dd0ee1cbbc66416bf9e7891b56f
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-11-10 13:01:33 +0530

    2.1.6

--- web/checkInDataBase.php
+++ web/checkInDataBase.php
@@ -10,6 +10,7 @@ $out_arr = array("present" => $user_present_in_db_false,
 "name" => null, 
 "reg_id" => null,
 "verified"  => null,
+"email" => null,
 "hash" => encrypt_decrypt($time,$GLOBALS['server_hash']));
 
 
@@ -21,13 +22,14 @@ $inpvehid = $_POST['vehid'];
 // echo "inpvehid: ".$inpvehid."<br>";
 // creatTempHash();
 
-    $querry = "SELECT count(*) as totalitems ,`name`,`email`,`reg_id`,`password`,`verified` FROM car_location WHERE `email` = '".$inpemail."' OR `reg_id` = '".$inpvehid."'";
+    $querry = "SELECT count(*) as totalitems ,`name`,`email`,`reg_id`,`verified` FROM car_location WHERE `email` = '".$inpemail."' OR `reg_id` = '".$inpvehid."'";
     if($result = mysqli_query($conn,$querry)){
         $row = mysqli_fetch_array($result);
         if($row['totalitems'] > 0){
             $out_arr["present"] = $user_present_in_db_true;
             $out_arr["name"] = $row['name'];
             $out_arr["reg_id"] = $row['reg_id'];
+            $out_arr["email"] = $row['email'];
             $out_arr["verified"] = $row['verified'];
             $out_arr["hash"] = $row['password'];
         }else{
